83d1ecf944c367ce517b97cb6e0f86709d9c7054
Leandro <leapalazzolo@gmail.com>
2023-02-27 18:48:23 -0300

    feat: define events

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -12,13 +12,19 @@ pub mod pallet {
 	pub struct Pallet<T>(_);
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
-	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 	}
-	#[pallet::event]   // <-- Step 3. code block will replace this.
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		/// Event emitted when a claim has been created.
+		ClaimCreated { who: T::AccountId, claim: T::Hash },
+		/// Event emitted when a claim is revoked by the owner.
+		ClaimRevoked { who: T::AccountId, claim: T::Hash },
+	}
 	#[pallet::error]   // <-- Step 4. code block will replace this.
 	#[pallet::storage] // <-- Step 5. code block will replace this.
 	#[pallet::call]    // <-- Step 6. code block will replace this.
