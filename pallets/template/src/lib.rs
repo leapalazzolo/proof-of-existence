f7fb65ec14219e472bd8b7e3cd74be7a134f9968
Leandro <leapalazzolo@gmail.com>
2023-02-27 18:45:52 -0300

    feat: define config

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -4,16 +4,22 @@ pub use pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-  use frame_support::pallet_prelude::*;
-  use frame_system::pallet_prelude::*;
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
 
-  #[pallet::pallet]
-  #[pallet::generate_store(pub(super) trait Store)]
-  pub struct Pallet<T>(_);
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
 
-  #[pallet::config]  // <-- Step 2. code block will replace this.
-  #[pallet::event]   // <-- Step 3. code block will replace this.
-  #[pallet::error]   // <-- Step 4. code block will replace this.
-  #[pallet::storage] // <-- Step 5. code block will replace this.
-  #[pallet::call]    // <-- Step 6. code block will replace this.
+	/// Configure the pallet by specifying the parameters and types on which it depends.
+	/// Configure the pallet by specifying the parameters and types on which it depends.
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		/// Because this pallet emits events, it depends on the runtime's definition of an event.
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+	}
+	#[pallet::event]   // <-- Step 3. code block will replace this.
+	#[pallet::error]   // <-- Step 4. code block will replace this.
+	#[pallet::storage] // <-- Step 5. code block will replace this.
+	#[pallet::call]    // <-- Step 6. code block will replace this.
 }
